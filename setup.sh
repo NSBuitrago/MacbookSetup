2c8ccd1e9a1d10aca3616f392a200d73188a4980
Nicolas Buitrago <nsbuitrago@icloud.com>
2022-01-20 12:16:46 -0600

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,6 @@ xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
 
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # pyenv
 brew install pyenv
 # python3 (update version if necessary)
@@ -15,6 +12,13 @@ pyenv install 3.9.0
 pyenv global 3.9.0
 pyenv version
 echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+
+# Go
+brew install go
+
+# R / RStudio
+brew install --cask r
+brew install --cask rstudio
 
 # git and github cli
 git config --gobal user.name nsbuitrago
@@ -25,12 +29,12 @@ gh auth login
 # apps
 brew install --cask iterm2
 brew install --cask 1password
-brew install --cask firefox
 brew install --cask obsidian
 brew install --cask visual-studio-code
 brew install --cask raycast
 brew install --cask notion
-brew install --cask mendeley
 brew install --cask zoom
+brew install --cask slack
 brew install --cask nordvpn
+brew install --cask blender
 
