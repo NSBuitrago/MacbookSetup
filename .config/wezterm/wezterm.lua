b304ffb288db68fa1d60a77534aecb53d52a7030
nsbuitrago <44626938+nsbuitrago@users.noreply.github.com>
2024-03-31 11:34:13 -0500

    update dots

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,30 @@
 local wezterm = require("wezterm")
 local config = {
 	font = wezterm.font("JetBrains Mono"),
-	color_scheme = "Gruvbox Material (Gogh)",
-	colors = {
-		background = "#1D2021",
-	},
+	color_scheme = "nord",
+	-- colors = {
+	-- 	background = "#1D2021",
+	-- },
 	enable_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
 	window_decorations = "RESIZE",
 	font_size = 16,
 	adjust_window_size_when_changing_font_size = false,
+	audible_bell = "Disabled",
+
+	keys = {
+		-- panes
+		{
+			key = "|",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "Enter",
+			mods = "CTRL|SHIFT",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+	},
 }
 
 return config
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
-xcode-select --install
+xcode-select --install # install developer tools
 
+# install homebrew package manager
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# install packages
 brew update
 brew install neovim
 brew install ripgrep
@@ -21,15 +23,16 @@ brew install fastfetch
 brew install htop
 brew install stow
 
+# install python and set global version
 pyenv install 3.10.0
 pyenv global 3.10.0
 pyenv version
 
-git config --gobal user.name nsbuitrago
-git config --global user.email mail@nsbuitrago.com
+# login to github cli and add extensions
 gh auth login
 gh extension install github/gh-copilot
 
+# install graphical applications
 brew install --cask arc
 brew install --cask wezterm
 brew install --cask raycast
@@ -37,8 +40,10 @@ brew install --cask obsidian
 brew install --cask microsoft-word
 brew install --cask microsoft-excel
 brew install --cask 1password
-brew install --cask affinity-designer
-brew install --cask affinity-photo
+# canva acquisition :(
+# brew install --cask affinity-designer
+# brew install --cask affinity-photo
+brew install --cask inkscape
 brew install --cask screen-studio
 brew install --cask zoom
 brew install --cask slack
@@ -46,9 +51,16 @@ brew install --cask blender
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask utm
 
+# install fonts
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono
 
+# set finder settings
 defaults write com.apple.finder showPathBar -bool true
 defaults write com.apple.finder showStatusBar -bool true
 
+# clone symlink dots
+cd ~
+gh repo clone nsbuitrago/.dots
+cd .dots
+stow .
